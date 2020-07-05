85c9095d28d473f138f8bda3347b8e555eefbfe8
Jerel Rocktaschel <jerel.rocktaschel@gmail.com>
2020-07-05 13:12:48 -0400

    Initial Commit

---
+++ BBallPlayerStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBallPlayerStats
+//
+//  Created by Jerel Rocktaschel rMBP on 7/5/20.
+//  Copyright © 2020 HighScoreApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BBallPlayerStatsTests/BBallPlayerStatsTests.swift
@@ -0,0 +1,34 @@
+//
+//  BBallPlayerStatsTests.swift
+//  BBallPlayerStatsTests
+//
+//  Created by Jerel Rocktaschel rMBP on 7/5/20.
+//  Copyright © 2020 HighScoreApps. All rights reserved.
+//
+
+import XCTest
+@testable import BBallPlayerStats
+
+class BBallPlayerStatsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
